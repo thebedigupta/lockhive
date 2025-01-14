d46b968140212ce9ae88ddd03b2c9f125c670f4b
Bedi Gupta <bedigupta76@gmail.com>
2025-01-14 10:48:29 +0530

    Improve database connection file

--- config/db.js
+++ config/db.js
@@ -1,19 +1,9 @@
 const mongoose = require('mongoose');
 
 function connectToDB() {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true, // Ensure indexes are created
-    })
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-      console.error('Could not connect to MongoDB', err);
-      process.exit(1); // Exit process with failure
-    });
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+  });
 }
 
 module.exports = connectToDB;
