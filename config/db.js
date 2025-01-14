f046d06fd5a6afaea4cd063de79b62019833c668
Bedi Gupta <bedigupta76@gmail.com>
2025-01-14 10:52:01 +0530

    Fixing error related to MongoDB

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectToDB() {
-  mongoose.connect(process.env.MONGO_URI).then(() => {
+  mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB');
   });
 }
